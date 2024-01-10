64f9179e4bc8d61149452d58aead0b5ef97f5de5
Daniel Ion <idani3093@gmail.com>
2024-01-10 02:49:39 +0200

    fully implemented teachable machine

--- machine.py
+++ machine.py
@@ -31,49 +31,60 @@ batch_size = 32
 img_height = 180
 img_width = 180
 
-def resize_images(uploaded_photos):
-    # resize each photo inside uploaded_photos
-    input_size = (img_height, img_width)
-    for sublist in uploaded_photos:
-        for photo in sublist:
-            photo = photo.resize(input_size, Image.BILINEAR)
-    return uploaded_photos    
+# === meant to be something ===
+# def resize_images(uploaded_photos):
+#     # resize each photo inside uploaded_photos
+#     input_size = (img_height, img_width)
+#     for sublist in uploaded_photos:
+#         for photo in sublist:
+#             photo = photo.resize(input_size, Image.BILINEAR)
+#     return uploaded_photos    
+# === meant to be something ===
 
 def preprocess(X_data, Y_data, num_classes):
     # convert images to numpy arrays and normalize pixel values
     all_data = []
     all_labels = []
-    x_data = []
     i = 0
 
     for photo in X_data:
         # transform bytes in rgb values
-        # pixel_array = np.array(photo)
-        numpy_array = np.frombuffer(photo, dtype = np.uint8)
-        normalize_image = numpy_array / 255.0
+        pixel_array = np.array(photo)
+
+        # === 
+        # numpy_array = np.frombuffer(photo, dtype = np.uint8)
+        # normalize_image = numpy_array / 255.0
+        # ===
+
+        normalize_image = pixel_array / 255.0
         all_data.append(normalize_image)
         all_labels.append(Y_data[i])
         i = i+1
 
-    for normalized in all_data:
-        x_data.append(np.array(normalized))
+    x_data = np.array (all_data)
     y_data = utils.to_categorical(all_labels, num_classes=num_classes)
     return x_data, y_data
 
 def train(target, uploaded_photos, machine_name, num_classes):
     # convert FileStorage to image and then resize it to the preferred sizes
     actual_photos = [[] for _ in range(num_classes)]
+    input_size = (img_height, img_width)
     for i in range(num_classes):
         for photo_bytes in uploaded_photos[i]:
-            actual_photos[i].append(photo_bytes.read())
+            image = Image.open(BytesIO(photo_bytes.read())).convert("RGB")
+            actual_photos[i].append(image.resize(input_size, Image.BILINEAR))
+            # actual_photos[i].append(Image.open(BytesIO(photo_bytes.read())).resize(input_size, Image.BILINEAR))
+    
+    # uploaded_photos = resize_images(uploaded_photos)
     x_data = []
     y_data = []
 
-    # failed attempt
+    # === failed attempt
     # train_data = utils.image_dataset_from_directory(uploaded_photos, validation_split = 0.2, subset = "training", seed = 123,
     #                                                 image_size = (img_height, img_width), batch_size=batch_size)
     # valid_Data = utils.image_dataset_from_directory(uploaded_photos, validation_split = 0.2, subset = "validation", seed = 123,
     #                                                 image_size = (img_height, img_width), batch_size=batch_size)
+    # ===
 
     # set labels
     for i in range(num_classes):
@@ -87,6 +98,7 @@ def train(target, uploaded_photos, machine_name, num_classes):
     rd.shuffle(temp)
     X_data, Y_data = zip(*temp)
 
+    # === failed attempt
     # # shuffle data & use 80% of images for training and 20% for validation
     # X_data = list(X_data)
     # Y_data = list(Y_data)
@@ -95,6 +107,7 @@ def train(target, uploaded_photos, machine_name, num_classes):
     # validX_data = X_data[index:]
     # trainY_data = Y_data[:index]
     # validY_data = Y_data[index:]
+    # ===
 
     x_train, y_train = preprocess(X_data, Y_data, num_classes)
     
